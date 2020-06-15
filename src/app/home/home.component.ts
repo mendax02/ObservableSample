02949a7a92371beaf3fc2ae9ed5d12f64fa3f4fd
mendax02 <siddharth.rawat@outlook.com>
2020-06-15 16:25:47 +0530

    custome observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,12 +16,36 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if ( count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('count greater than 3'));
+        }
+        count++;
+      }, 1000);
     });
-      }
 
-      ngOnDestroy(): void {
-        this.firstObsSubscription.unsubscribe();
-      }
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+        console.log('Completed!');
+    }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+
+  }
 }
