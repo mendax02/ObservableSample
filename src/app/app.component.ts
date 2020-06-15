ac43e4d4573c02e5431555fe3f14a1b0c0148d5e
mendax02 <siddharth.rawat@outlook.com>
2020-06-15 21:05:51 +0530

    subjects in place of eventEmitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
+  constructor(private userService: UserService) {}
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 
-  ngOnInit() {}
+
+  ngOnInit() {
+   this.activatedSub = this.userService.activatedEmitter.subscribe((didActivated: boolean) => {
+      this.userActivated = didActivated;
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -35,7 +35,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.pipe(map((data: number) => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return +data > 0;
+    }), map((data: number) => {
       return 'Round: ' + (data + 1);
     })).subscribe(data => {
       console.log(data);
