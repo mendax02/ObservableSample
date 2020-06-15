08bea49061a31edc72877bd86e23bce053edeccc
mendax02 <siddharth.rawat@outlook.com>
2020-06-15 20:30:36 +0530

    operators | pipes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
 
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -23,7 +25,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if ( count === 2) {
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
@@ -33,13 +35,15 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
       alert(error.message);
     }, () => {
-        console.log('Completed!');
+      console.log('Completed!');
     }
     );
   }
